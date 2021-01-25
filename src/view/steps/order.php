0ca3091c423bddc714bb2633975ef48107bc9687
Pieter Olieiver <pieter.olievier@student.howest.be>
2021-01-25 20:13:47 +0100

    responsive order page

--- src/view/steps/order.php
+++ src/view/steps/order.php
@@ -22,6 +22,7 @@
         </div>
 
 <div class="order-grid">
+    <!-- <div class="user-section"> -->
 
     <p class="order-title">bestel jouw kit</p>
 
@@ -34,7 +35,8 @@
     <form action="index.php?page=order" method="post" class="order-form">
         <p class="form-greeting">aangenaam</p>
   
-        <label class="order-label" for="naam">Naam</br>
+        <label class="order-label" for="naam">
+            <p class="order-label__label">Naam</p>
                     <input class="order-input" type="text" id="naam" name="naam"  required value="<?php if (!empty($_POST['naam'])) {
                                                                                                                                                             echo $_POST['naam'];
                                                                                                                                                         } ?>">
@@ -42,7 +44,8 @@
                     
         </label>
 
-        <label class="order-label" for="voornaam">Voornaam</br>
+        <label class="order-label" for="voornaam">
+             <p class="order-label__label">Voornaam</p>
                     <input class="order-input" type="text" id="voornaam" name="voornaam"  required value="<?php if (!empty($_POST['voornaam'])) {
                                                                                                                                                             echo $_POST['voornaam'];
                                                                                                                                                         } ?>">
@@ -50,19 +53,24 @@
                     
         </label>
 
-        <label class="order-label" for="email">email</br>
+        <label class="order-label" for="email">
+             <p class="order-label__label">email</p>
                     <input class="order-input" type="email" id="email" name="email"  required value="<?php if (!empty($_POST['email'])) {
                                                                                                                                                             echo $_POST['email'];
                                                                                                                                                         } ?>">
 
                     
         </label>
+        <!-- </div> -->
+
+        <!-- <div class="adress-section"> -->
 
         <p class="order-form__adress">Waar mogen wij jouw kit leveren?</p>
 
        <div class="order-adres-number">                                                                                                                                                 
 
-        <label  for="adres">adres</br>
+        <label  for="adres">
+             <p class="order-label__label">adres</p>
                     <input class="order-input" type="text" id="adres" name="adres"  required value="<?php if (!empty($_POST['adres'])) {
                                                                                                                                                             echo $_POST['adres'];
                                                                                                                                                         } ?>">
@@ -81,7 +89,8 @@
 
         <div class="order-code-city">
 
-        <label class="order-label" for="postcode">postcode</br>
+        <label class="order-label" for="postcode">
+             <p class="order-label__label">postcode</p>
                     <input class="order-input__code" type="text" id="postcode" name="postcode"  required value="<?php if (!empty($_POST['postcode'])) {
                                                                                                                                                             echo $_POST['postcode'];
                                                                                                                                                         } ?>">
@@ -89,7 +98,8 @@
                     
         </label>
 
-        <label class="order-label" for="stad">stad/gemeente</br>
+        <label class="order-label" for="stad">
+             <p class="order-label__label-city">stad/gemeente</p>
                     <input class="order-input__city" type="text" id="stad" name="stad"  required value="<?php if (!empty($_POST['stad'])) {
                                                                                                                                                             echo $_POST['stad'];
                                                                                                                                                         } ?>">
@@ -103,7 +113,7 @@
                 <input type="submit" value="registreer" class="register-submit">
             </div>
         </div>
-        
+        <!-- </div> -->
     </form>
 
 
